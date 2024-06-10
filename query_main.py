a30a5f4e16158ecc3b064df9e7546fbf4207d5b3
Saleminezhad <129303520+Saleminezhad@users.noreply.github.com>
2024-06-10 12:13:15 -0400

    Update query_main.py

--- query_main.py
+++ query_main.py
@@ -11,7 +11,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 def main(var, K, p, total_run, epsilon, metric_name, network_size, model_name):
     
 
-    data_folder='/home/abbas/ADG-QPP/msmarco-data/indices/'
+    data_folder='/msmarco-data/indices/'
     index= faiss.read_index(os.path.join(data_folder,model_name))
     
     correlation_list = ""
@@ -19,22 +19,22 @@ def main(var, K, p, total_run, epsilon, metric_name, network_size, model_name):
 
     for year in years:
         
-        main_query_path = f'/home/abbas/ADG-QPP/msmarco-data/trec_data/{year}/{year}_queries'
+        main_query_path = f'/msmarco-data/trec_data/{year}/{year}_queries'
 
-        NNQ_query_path_emb =f"/home/abbas/ADG-QPP/NNQ/output/top_100_emb_sim_train_{year}_queries_msmarco-MiniLM-L6-cos-v5_matched_queries.json"
+        NNQ_query_path_emb =f"/NNQ/output/top_100_emb_sim_train_{year}_queries_msmarco-MiniLM-L6-cos-v5_matched_queries.json"
         with open(NNQ_query_path_emb, 'r') as json_file:
             NNQ_data_emd_similarity = json.load(json_file)
         
         
-        noisy_run_path = f'/home/abbas/ADG-QPP/run/{metric_name}_{network_size}_{epsilon}_{var}/{metric_name}_{year}_{var}_Noisyq_run.tsv'
+        noisy_run_path = f'/run/{metric_name}_{network_size}_{epsilon}_{var}/{metric_name}_{year}_{var}_Noisyq_run.tsv'
         
         # write a code to check if a folder exits or not, if not create it.
-        folder_path = f'/home/abbas/ADG-QPP/run/{metric_name}_{network_size}_{epsilon}_{var}'
+        folder_path = f'/run/{metric_name}_{network_size}_{epsilon}_{var}'
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
         
-        if not os.path.exists(f'/home/abbas/ADG-QPP/RBO/{metric_name}_{network_size}_{epsilon}_{var}'):
-            os.makedirs(f'/home/abbas/ADG-QPP/RBO/{metric_name}_{network_size}_{epsilon}_{var}')
+        if not os.path.exists(f'/RBO/{metric_name}_{network_size}_{epsilon}_{var}'):
+            os.makedirs(f'/RBO/{metric_name}_{network_size}_{epsilon}_{var}')
             
         
         start_time = time.time()
@@ -44,13 +44,13 @@ def main(var, K, p, total_run, epsilon, metric_name, network_size, model_name):
             retrieve_noisy_query_network_v1_emb(NNQ_data_emd_similarity, noisy_run_path, main_query_path, index, var, epsilon, metric_name, network_size, year)
 
 
-            RBO_output_path = f'/home/abbas/ADG-QPP/RBO/{metric_name}_{network_size}_{epsilon}_{var}/{n}_{year}_{var}_rbo_noisy_run_fil_{K}'
+            RBO_output_path = f'/RBO/{metric_name}_{network_size}_{epsilon}_{var}/{n}_{year}_{var}_rbo_noisy_run_fil_{K}'
 
-            orig_run_path = f'/home/abbas/ADG-QPP/run/org_{year}.tsv'
+            orig_run_path = f'/run/org_{year}.tsv'
 
             RBO_func(orig_run_path, noisy_run_path, RBO_output_path, year, p, K )
             
-            performance_path = f'/home/abbas/ADG-QPP/performance/ndcg_cut_10_dl{year}.tsv'
+            performance_path = f'/performance/ndcg_cut_10_dl{year}.tsv'
             pears, kendal, spear = correlation_eval(performance_path, RBO_output_path)
 
             correlation_list += f'{n} {year} {pears} {kendal} {spear} \n'
@@ -61,10 +61,10 @@ def main(var, K, p, total_run, epsilon, metric_name, network_size, model_name):
         end_time1 = time.time()
         print(f"{metric_name} {year}  :", end_time1 - start_time)
     
-    if not os.path.exists(f"/home/abbas/ADG-QPP/correlations/{metric_name}"):
-        os.makedirs(f"/home/abbas/ADG-QPP/correlations/{metric_name}")
+    if not os.path.exists(f"/correlations/{metric_name}"):
+        os.makedirs(f"/correlations/{metric_name}")
 
-    with open(f"/home/abbas/ADG-QPP/correlations/{metric_name}/{metric_name}_{network_size}_{epsilon}_{var}.txt", 'w') as out:
+    with open(f"/correlations/{metric_name}/{metric_name}_{network_size}_{epsilon}_{var}.txt", 'w') as out:
         out.write(correlation_list)
             
             
